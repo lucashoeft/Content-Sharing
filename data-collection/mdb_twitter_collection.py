0c5bc7d941e4e22df45c7c4a2cf58d259a58bd53
Lucas Hoeft <26332559+lucashoeft@users.noreply.github.com>
2022-06-25 17:10:37 +0200

    Add secondary accounts
    Add secondary accounts of Baerbock and Scholz including friendships, tweets and account description

--- data-collection/mdb_twitter_collection.py
+++ data-collection/mdb_twitter_collection.py
@@ -2,8 +2,11 @@ import pandas as pd
 
 mdb_list = pd.read_csv('../data/processed/mdb_list/mdb_list.csv', sep=";")
 twitter_usernames = pd.read_csv('../data/external/twitter_usernames.csv', sep=";")
+twitter_usernames_secondary = pd.read_csv('../data/external/twitter_usernames_secondary.csv', sep=";")
 
 # Merge mdb list with info about twitter usernames
 mdb_twitter_list = pd.merge(mdb_list, twitter_usernames, on='bundestag_id')
+mdb_twitter_list_secondary = pd.merge(mdb_list, twitter_usernames_secondary, on='bundestag_id')
 
 mdb_twitter_list.to_csv('../data/processed/mdb_twitter_list/mdb_twitter_list.csv', index=False, sep=";")
+mdb_twitter_list_secondary.to_csv('../data/processed/mdb_twitter_list/mdb_twitter_list_secondary.csv', index=False, sep=";")
